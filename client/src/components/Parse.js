154adb8f6204e9784b2387afa26c08a9e3fcfd68
kevin11-code <kevinharia11@gmail.com>
2023-02-18 07:13:33 +0530

    minor changes

--- client/src/components/Parse.js
+++ client/src/components/Parse.js
@@ -35,75 +35,77 @@ function Parse() {
             Upload the Resumes of your team members
           </p>
         </div>
-        <div className="flex flex-wrap">
-          <div></div>
-          <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-            <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-              Member 1
-            </h2>
-            <label>
-              Choose 1st file:
-              <input
-                className="bg-slate-200 rounded-lg pl-2 py-2 inline-flex items-center"
-                type="file"
-                accept="application/pdf"
-                onChange={handleFileChange}
-              />
-            </label>
-          </div>
-          <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-            <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-              Member 2
-            </h2>
-            <label>
-              Choose 2nd file:
-              <input
-                className="bg-slate-200 rounded-lg pl-2 py-2 inline-flex items-center"
-                type="file"
-                accept="application/pdf"
-                onChange={handleFileChange}
-              />
-            </label>
-          </div>
-          <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-            <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-              Member 3
-            </h2>
-            <label>
-              Choose 1st file:
-              <input
-                className="bg-slate-200 rounded-lg pl-2 py-2 inline-flex items-center"
-                type="file"
-                accept="application/pdf"
-                onChange={handleFileChange}
-              />
-            </label>
-          </div>
-        </div>
-        <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-          <form onSubmit={handleSubmit}>
-            <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-              Member 4
-            </h2>
-            <label>
-              Choose 4th file:
-              <input
-                className="bg-slate-200 rounded-lg pl-2 py-2 inline-flex items-center"
-                type="file"
-                accept="application/pdf"
-                onChange={handleFileChange}
-              />
-            </label>
 
-            {/* // <button type="submit">Upload</button> */}
+        <div>
+          <div className="flex-1 ">
+            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+              <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
+                Member 1
+              </h2>
+              <label>
+                Choose 1st file:
+                <input
+                  className="bg-slate-200 rounded-lg pl-2 py-2 inline-flex items-center"
+                  type="file"
+                  accept="application/pdf"
+                  onChange={handleFileChange}
+                />
+              </label>
+            </div>
+            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+              <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
+                Member 2
+              </h2>
+              <label>
+                Choose 2nd file:
+                <input
+                  className="bg-slate-200 rounded-lg pl-2 py-2 inline-flex items-center"
+                  type="file"
+                  accept="application/pdf"
+                  onChange={handleFileChange}
+                />
+              </label>
+            </div>
+            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+              <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
+                Member 3
+              </h2>
+              <label>
+                Choose 1st file:
+                <input
+                  className="bg-slate-200 rounded-lg pl-2 py-2 inline-flex items-center"
+                  type="file"
+                  accept="application/pdf"
+                  onChange={handleFileChange}
+                />
+              </label>
+            </div>
+          </div>
+          <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+            <form onSubmit={handleSubmit}>
+              <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
+                Member 4
+              </h2>
+              <label>
+                Choose 4th file:
+                <input
+                  className="bg-slate-200 rounded-lg pl-2 py-2 inline-flex items-center"
+                  type="file"
+                  accept="application/pdf"
+                  onChange={handleFileChange}
+                />
+              </label>
 
-            <button
-              type="submit"
-              className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-            >
-              Upload
-            </button>
-          </form>
+              {/* // <button type="submit">Upload</button> */}
+
+              <button
+                type="submit"
+                className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+              >
+                Upload
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </section>
